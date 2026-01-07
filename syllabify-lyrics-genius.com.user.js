3c28044a1ab1760129b8f838a1ea061e7dcb91fd
りしき <lisiki.jp@gmail.com>
2026-01-08 07:47:26 +0900

    Update syllabify-lyrics-genius.com.user.js

--- syllabify-lyrics-genius.com.user.js
+++ syllabify-lyrics-genius.com.user.js
@@ -10,6 +10,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=genius.com
 // @grant        none
 // @run-at       document-end
+// ==UserScript==
 
 (function() {
     'use strict';
